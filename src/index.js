99e57654fa11dd93fbdfbc76f15efb45903e5b35
AstridGMC <astrid_martinez60@yahoo.es>
2019-10-08 18:47:16 -0600

    actualizacion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("hola");
+// imports 
+const path = require('path');
+
+//frameworks
+const express = require('express');
+const app = express();
+var publicPath = path.resolve(__dirname, 'public'); 
+
+
+// configuraciones
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+app.set('port', 3000);
+
+//rutas
+app.use(require('./routes/index'));
+
+app.use(express.static(publicPath)); 
+//escucha
+
+app.listen(app.get('port'), ()=> {
+    console.log('escuchando e el puerto ' , app.get('port'));
+
+}); 
---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+
+router.get('/',(req,res)=> {
+    res.render('Inicio',{mas:34})
+})
+
+//rutas
+router.get('/Inicio',(req, res)=> {
+    res.render(path.join( __dirname,'../views/index.ejs'))
+});
+
+router.get('/header',(req, res)=> {
+    res.sendFile(path.join( __dirname,'../public/header.html'))
+});
+
+module.exports=router;
