dddeaff1db935ca8b013a1f42624824db761c7b6
AstridGMC <astrid_martinez60@yahoo.es>
2019-10-09 20:47:17 -0600

    actualizacion

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,6 +12,10 @@ router.get('/Inicio',(req, res)=> {
     res.render(path.join( __dirname,'../views/index.ejs'))
 });
 
+router.get('/insertarArchivo',(req, res)=> {
+    res.render(path.join( __dirname,'../views/insertarArchivo.ejs'))
+});
+
 router.get('/header',(req, res)=> {
     res.sendFile(path.join( __dirname,'../public/header.html'))
 });
